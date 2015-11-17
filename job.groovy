4e8bbf17ce8d285198236efda46f0d47265131f6
Michael Richard <michael.richard@gettyimages.com>
2015-11-17 11:46:59 -0800

    Moar syntax

--- job.groovy
+++ job.groovy
@@ -1,16 +1,18 @@
 job('seed-job') {
     scm {
-         remote {
+      git{
+           remote {
               url('https://github.com/michaelarichard/jenkins_dsl.git')
-         }
+           }
+	 }
     }
     triggers {
         scm('* * * * *')
     }
     steps {
         dsl {
-//            text(readFileFromWorkspace('job.groovy'))
-	      external('job.groovy')
+            text(readFileFromWorkspace('job.groovy'))
+//	      external('job.groovy')
 //            removeAction('DELETE')
 //            removeViewAction('DELETE')
         }
