733995bd83126e7777e9655e68462ffaf4a3a910
Michael Richard <michael.richard@gettyimages.com>
2016-01-12 07:58:00 -0800

    Updating job to shell script

--- jobs/ops/flush_memcache/job.groovy
+++ jobs/ops/flush_memcache/job.groovy
@@ -2,12 +2,7 @@ job('flush_memcache') {
     triggers {
     }
     steps {
-        dsl {
-            text(readFileFromWorkspace('jobs/ops/flush_memcache/job.sh'))
-        }
+    shell readFileFromWorkspace('jobs/ops/flush_memcache/job.sh')
     }
 }
-
-// example of how to include
-//evaluate(new File("../jobs/job1.groovy"))
 
