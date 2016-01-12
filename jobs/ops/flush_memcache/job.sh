d9c10e3d2c0934f640b6e5426c067a34b45e2c0a
Michael Richard <michael.richard@gettyimages.com>
2016-01-12 08:04:17 -0800

    Fixing memcache flush job

--- jobs/ops/flush_memcache/job.sh
+++ jobs/ops/flush_memcache/job.sh
@@ -1,6 +1,7 @@
 #/bin/sh
-LUST=sea-dev3-vmc-default1.istockphoto.com
+LIST=sea-dev3-vmc-default1.istockphoto.com
 #LIST=`echo "select IP from siteenv.tbl_SS_Server where Type='memcache';" | mysql -N | grep 10.223` 
+yum install -y nc
 for i in $LIST ; do 
 echo "flush_all" | nc $i 11211
 done
